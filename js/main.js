d45c71400e362df959d179ce61c355668f1db177
azrizabidi <ahmadazri92@gmail.com>
2020-06-19 10:29:48 +0800

    insert menu to all page

--- js/main.js
+++ js/main.js
@@ -1,40 +1,33 @@
-function togNav() {
-  var nav = document.getElementById("mySidenav");
-  var main = document.getElementById("main");
-  var menu = document.getElementById("menu");
-  if (nav.style.width == '80px') {
-    nav.style.width = '0';
-    main.style.marginLeft = '0';
-    menu.classList.add("active");
-  } else {
-    nav.style.width = "80px";
-    main.style.marginLeft = '80px';
-    menu.classList.remove("active");
-  }
-}
-
-// function secNav() {
-//   var nav = document.getElementById("secnav");
-//   var main = document.getElementById("main");
-//   var menu = document.getElementById("menu");
-//   if (nav.style.width == '80px') {
-//     nav.style.width = '0';
-//     main.style.marginLeft = '0';
-//     menu.classList.add("active");
-//   } else {
-//     nav.style.width = "80px";
-//     main.style.marginLeft = '80px';
-//     menu.classList.remove("active");
-//   }
-// }
-
-
-// $( "#menuicon" ).click(function() {
-//         $(this).find(".icon").toggleClass('fa-bars fa-stream',1500);
-//     });
-
-$(document).ready(function () {
-  $('#mySidenav dropdown a').on('click', function () {
-      $('#mySidenav').toggleClass('expand');
-  });
+$(document).ready(function(){
+   $('.sidebar a.collapsed').click( function(e) {
+        $('.fa-chevron-left').addClass('fa-rotate-45');
+        $('.collapse').collapse('hide').delay(1000).queue(function(next){
+           $('.collapse.show').collapse('show');
+           next();
+       });
+    });
+    $("#btn-sidebar").click(function(){
+      if ($(".sidebar").hasClass('close')) {
+         $(".sidebar").removeClass("close w-0").addClass("overflow-hidden");
+         $("#main").removeClass('main-lg').delay(1000).queue(function(next){
+            $('.sidebar').removeClass("overflow-hidden");
+            next();
+        });
+      } else if ($(".sidebar").hasClass('open')) {
+          $(".sidebar").addClass('w-0 overflow-hidden').delay(1000).queue(
+            function(next) {
+              $(".sidebar").removeClass("open overflow-hidden").addClass('close');
+              $(".sub-menu.show").collapse('hide');
+              $('#main').removeClass("main-sm").addClass('main-lg');
+             next();
+         });
+       } else {
+           $("#main").addClass('main-sm');
+           $('.sidebar').addClass("open overflow-hidden").delay(1000).queue(
+             function(next) {
+              $('.sidebar').removeClass("overflow-hidden");
+              next();
+          });
+       }
+    });
 });
